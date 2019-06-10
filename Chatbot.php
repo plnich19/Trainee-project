67fb289e03d0d148fe54c4a87dc1080239395983
Nichakan Udomlerkchai <PloyNichakan@gmail.com>
2019-06-10 13:41:39 +0700

    Update Chatbot.php
    เพิ่ม json ของ FLEX MESSEGE

--- Chatbot.php
+++ Chatbot.php
@@ -6,6 +6,94 @@
     $datas = file_get_contents('php://input');
     
     // {
+    //     "type": "flex",
+    //     "altText": "Flex Message",
+    //     "contents": {
+    //       "type": "bubble",
+    //       "header": {
+    //         "type": "box",
+    //         "layout": "horizontal",
+    //         "contents": [
+    //           {
+    //             "type": "text",
+    //             "text": "Lottery Result 1 January, 2019",
+    //             "flex": 4,
+    //             "size": "sm",
+    //             "weight": "bold",
+    //             "color": "#AAAAAA"
+    //           }
+    //         ]
+    //       },
+    //       "hero": {
+    //         "type": "image",
+    //         "url": "https://s.isanook.com/mn/0/rp/r/w700/ya0xa0m1w0/aHR0cHM6Ly9zLmlzYW5vb2suY29tL21uLzAvdWQvMTA3LzUzOTc3Ny9pc3RvY2stNDU4NjU4MzA1LmpwZw==.jpg",
+    //         "size": "full",
+    //         "aspectRatio": "20:13",
+    //         "aspectMode": "cover",
+    //         "action": {
+    //           "type": "uri",
+    //           "label": "Action",
+    //           "uri": "https://linecorp.com/"
+    //         }
+    //       },
+    //       "body": {
+    //         "type": "box",
+    //         "layout": "horizontal",
+    //         "spacing": "md",
+    //         "contents": [
+    //           {
+    //             "type": "box",
+    //             "layout": "vertical",
+    //             "flex": 1,
+    //             "contents": [
+    //               {
+    //                 "type": "image",
+    //                 "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/02_1_news_thumbnail_1.png",
+    //                 "gravity": "bottom",
+    //                 "size": "sm",
+    //                 "aspectRatio": "4:3",
+    //                 "aspectMode": "cover"
+    //               },
+    //               {
+    //                 "type": "image",
+    //                 "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/02_1_news_thumbnail_2.png",
+    //                 "margin": "md",
+    //                 "size": "sm",
+    //                 "aspectRatio": "4:3",
+    //                 "aspectMode": "cover"
+    //               }
+    //             ]
+    //           },
+    //           {
+    //             "type": "box",
+    //             "layout": "vertical",
+    //             "flex": 2,
+    //             "contents": [
+    //               {
+    //                 "type": "text",
+    //                 "text": "7 Things to Know for Today",
+    //                 "flex": 1,
+    //                 "size": "xs",
+    //                 "gravity": "top"
+    //               },
+    //               {
+    //                 "type": "separator"
+    //               },
+    //               {
+    //                 "type": "text",
+    //                 "text": "Hay fever goes wild",
+    //                 "flex": 2,
+    //                 "size": "xs",
+    //                 "gravity": "center"
+    //               }
+    //             ]
+    //           }
+    //         ]
+    //       }
+    //     }
+    //   }
+
+    // {
     //     "events":[
     //         {
     //             "type":"message","replyToken":"7d7d02d64b954dfcb93683de8cc41f5f",
@@ -316,4 +404,3 @@
 		return $datas;
     }
 ?>
-
